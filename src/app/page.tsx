8404358d7a232f739ddf2f128fc06e1f2e7666b5
Luis Romero <luiseduromp@hotmail.com>
2025-04-22 11:39:01 +0200

    Updated index page to use global state

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -8,26 +8,30 @@ import { JobPostInterface } from "@/types/jobPost";
 import { useEffect } from "react";
 
 export default function Home() {
-  const { jobPosts, setJobPosts } = useJobStore()
+  const { setJobPosts, filters } = useJobStore()
+
+  const fetchData = async () => {
+    const jobs: JobPostInterface[] = await fetchJobs(filters)
+    setJobPosts(jobs)
+  }
 
   useEffect(() => {
-    const fetchData = async () => {
-      const jobs: JobPostInterface[] = await fetchJobs()
-      setJobPosts(jobs)
-    }
     fetchData()
-  }, [])
+  }, [filters])
+
 
   return (
     <div >
-      <section className="w-full md:w-5/6 xl:w-3/4 mx-auto py-10">
+      <section className="w-full px-8 md:px-0 md:w-5/6 xl:w-3/4 mx-auto py-10">
         <h1 className="text-4xl md:text-6xl lg:text-8xl font-semibold inline-block bg-linear-65 from-purple-500 to-pink-500 text-transparent bg-clip-text">WorkHub</h1>
         <h4 className="text-2xl text-neutral-400">The place to find you dream job</h4>
       </section>
 
-      <section className="w-full md:w-5/6 xl:w-3/4 mx-auto my-8">
-        <SearchBar />
-        <JobList jobs={jobPosts} />
+      <SearchBar />
+
+      <section className="w-full px-8 md:px-0 md:w-5/6 xl:w-3/4 mx-auto my-8">
+        
+        <JobList />
       </section>
       
     </div>
