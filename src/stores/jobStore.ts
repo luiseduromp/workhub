66810cafa45993265d61621393d680605e4c91b0
Luis Romero <luiseduromp@hotmail.com>
2025-04-22 11:42:50 +0200

    Updated jobStore to use filters

--- src/stores/jobStore.ts
+++ src/stores/jobStore.ts
@@ -1,16 +1,28 @@
+import axios from "axios"
 import { create } from "zustand"
-
 import { JobPostInterface } from "@/types/jobPost"
 
 type JobStore = {
     jobPosts: JobPostInterface[]
+    filters: FilterInterface
     setJobPosts: (jobPosts: JobPostInterface[]) => void
-    getJobById: (id: Number) => JobPostInterface | undefined
+    getJobById: (id: Number) => JobPostInterface | undefined   
+    setFilters: (filters: Partial<FilterInterface>) => void
+}
+
+export interface FilterInterface {
+    tag: string
+    geo: string
+    industry: string
 }
 
 export const useJobStore = create<JobStore>((set, get) => ({
     jobPosts: [],
+    filters: { tag: '', geo: '', industry: '' },
     setJobPosts: (jobPosts) => set({ jobPosts }),
+    setFilters: (filters) => {
+        set((state) => ({ filters: { ...state.filters, ...filters } }))
+    },
     getJobById: (id:Number) => {
         const { jobPosts } = get()
         return jobPosts.find((job) => job.id === id)
